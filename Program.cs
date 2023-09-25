e613302b1d1301a431554f3318c7c325d7f457e7
abmusawer-f <abmusawer.furmoly@gmail.com>
2023-09-25 15:29:34 +0430

    done for the day

--- Program.cs
+++ Program.cs
@@ -1,3 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //#error version
 Console.WriteLine("Hello, World!");
+string horizontalLine = new('-', count: 74);
+Console.WriteLine(horizontalLine);
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+string grinningEmoji = char.ConvertFromUtf32(0x1F600);
+Console.WriteLine(grinningEmoji);
+
+string xml = """
+ <person age="50">
+ <first_name>Mark</first_name>
+ </person>
+ """;
+Console.WriteLine(xml);
+
+//page 112
+var person = new { FirstName = "Alice", Age = 56 };
+string json = $$"""
+ {
+ "first_name": "{{person.FirstName}}",
+ "age": {{person.Age}},
+ "calculation", "{{{1 + 2}}}"
+ }
+ """;
+Console.WriteLine(json);
+
+HttpClient client = new();
+
+HttpResponseMessage response =
+ await client.GetAsync("http://www.apple.com/");
+
+WriteLine("Apple's home page has {0:N0} bytes.",
+ response.Content.Headers.ContentLength);
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ﻿CSharp.11.and.NET.7.Modern.Cross-Platform.Development.Fundamentals.7th.Edition.Mark.J.Price
 
 
-page 107
+page 147
